30d790436fc6eb58f88354ff4255d7cfc8ccfb3b
Shadab Akhtar <shadab1996@gmail.com>
2021-10-09 01:16:53 +0530

    to get response

---
+++ src/main/java/com/shadabdsw/thymeleafdemo/Model/ServiceResponse.java
@@ -0,0 +1,16 @@
+package com.shadabdsw.thymeleafdemo.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ServiceResponse<T> {
+    private String status;
+    private T data;
+    
+}
