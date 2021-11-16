8db960b62a0f5f8e7e295f3ee0be8ceec1e3d5f4
Shadab Akhtar <shadab1996@gmail.com>
2021-11-16 18:29:15 +0530

    empty constructor

--- src/main/java/com/shadabdsw/thymeleafdemo/Model/Vaccination.java
+++ src/main/java/com/shadabdsw/thymeleafdemo/Model/Vaccination.java
@@ -17,6 +17,9 @@ public class Vaccination {
         this.vaccinationDate = vaccinationDate;
         this.nextVaccinationDate = nextVaccinationDate;
     }
+
+    public Vaccination() {
+    }
     
 
     public String getVaccinationType() {
