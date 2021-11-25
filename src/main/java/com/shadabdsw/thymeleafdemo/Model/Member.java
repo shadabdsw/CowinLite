7b74f2c3a2e96c2af3375f952c5fe92d3739ad68
Shadab Akhtar <shadab1996@gmail.com>
2021-11-25 15:26:39 +0530

    dob to yob

--- src/main/java/com/shadabdsw/thymeleafdemo/Model/Member.java
+++ src/main/java/com/shadabdsw/thymeleafdemo/Model/Member.java
@@ -3,28 +3,26 @@ package com.shadabdsw.thymeleafdemo.Model;
 import java.util.Date;
 import java.util.List;
 
-import org.springframework.data.annotation.Transient;
-
 public class Member {
     
     private String phnNumber;
     private String adhaar;
     private String name;
     private String gender;
-    private Date dob;
+    private String yob;
     private String vaccinationStatus;
     private List<Vaccination> vaccine;
 
     public Member() {
     }
 
-    public Member(String phnNumber, String adhaar, String name, String gender, Date dob, String vaccinationStatus,
+    public Member(String phnNumber, String adhaar, String name, String gender, String yob, String vaccinationStatus,
             List<Vaccination> vaccine) {
         this.phnNumber = phnNumber;
         this.adhaar = adhaar;
         this.name = name;
         this.gender = gender;
-        this.dob = dob;
+        this.yob = yob;
         this.vaccinationStatus = vaccinationStatus;
         this.vaccine = vaccine;
     }
@@ -41,12 +39,12 @@ public class Member {
         return adhaar;
     }
 
-    public Date getDob() {
-        return dob;
+    public String getYob() {
+        return yob;
     }
 
-    public void setDob(Date dob) {
-        this.dob = dob;
+    public void setYob(String yob) {
+        this.yob = yob;
     }
 
     public String getGender() {
@@ -87,8 +85,8 @@ public class Member {
 
     @Override
     public String toString() {
-        return "Member [adhaar=" + adhaar + ", dob=" + dob + ", gender=" + gender + ", name=" + name + ", phnNumber="
-                + phnNumber + ", vaccinationStatus=" + vaccinationStatus + ", vaccine=" + vaccine + "]";
+        return "Member [adhaar=" + adhaar + ", gender=" + gender + ", name=" + name + ", phnNumber=" + phnNumber
+                + ", vaccinationStatus=" + vaccinationStatus + ", vaccine=" + vaccine + ", yob=" + yob + "]";
     }
 
 }
