665d9d60a1c579e5d250d6b48922a1aff29b1d0c
Shadab Akhtar <shadab1996@gmail.com>
2021-11-26 19:57:17 +0530

    name to memberName

--- src/main/java/com/shadabdsw/thymeleafdemo/Model/Member.java
+++ src/main/java/com/shadabdsw/thymeleafdemo/Model/Member.java
@@ -1,13 +1,12 @@
 package com.shadabdsw.thymeleafdemo.Model;
 
-import java.util.Date;
 import java.util.List;
 
 public class Member {
     
     private String phnNumber;
     private String adhaar;
-    private String name;
+    private String memberName;
     private String gender;
     private String yob;
     private String vaccinationStatus;
@@ -16,11 +15,11 @@ public class Member {
     public Member() {
     }
 
-    public Member(String phnNumber, String adhaar, String name, String gender, String yob, String vaccinationStatus,
+    public Member(String phnNumber, String adhaar, String memberName, String gender, String yob, String vaccinationStatus,
             List<Vaccination> vaccine) {
         this.phnNumber = phnNumber;
         this.adhaar = adhaar;
-        this.name = name;
+        this.memberName = memberName;
         this.gender = gender;
         this.yob = yob;
         this.vaccinationStatus = vaccinationStatus;
@@ -55,12 +54,12 @@ public class Member {
         this.gender = gender;
     }
 
-    public String getName() {
-        return name;
+    public String getMemberName() {
+        return memberName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
     }
 
     public void setAdhaar(String adhaar) {
@@ -85,8 +84,9 @@ public class Member {
 
     @Override
     public String toString() {
-        return "Member [adhaar=" + adhaar + ", gender=" + gender + ", name=" + name + ", phnNumber=" + phnNumber
-                + ", vaccinationStatus=" + vaccinationStatus + ", vaccine=" + vaccine + ", yob=" + yob + "]";
+        return "Member [adhaar=" + adhaar + ", gender=" + gender + ", memberName=" + memberName + ", phnNumber="
+                + phnNumber + ", vaccinationStatus=" + vaccinationStatus + ", vaccine=" + vaccine + ", yob=" + yob
+                + "]";
     }
 
 }
