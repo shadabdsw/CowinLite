f966aef81fb8f5b29c5fe9aafe680c47329494bc
Shadab Akhtar <shadab1996@gmail.com>
2021-12-15 17:22:36 +0530

    rest template addition

--- src/main/java/com/shadabdsw/thymeleafdemo/ThymeleafdemoApplication.java
+++ src/main/java/com/shadabdsw/thymeleafdemo/ThymeleafdemoApplication.java
@@ -2,9 +2,16 @@ package com.shadabdsw.thymeleafdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ThymeleafdemoApplication {
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafdemoApplication.class, args);
