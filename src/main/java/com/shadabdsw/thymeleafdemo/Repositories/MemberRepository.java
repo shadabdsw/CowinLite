55d7d265a9ef347a2ef074c8611d1d3a0f559917
Shadab Akhtar <shadab1996@gmail.com>
2022-01-03 18:02:43 +0530

    not needed

--- src/main/java/com/shadabdsw/thymeleafdemo/Repositories/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shadabdsw.thymeleafdemo.Repositories;
-
-import com.shadabdsw.thymeleafdemo.Model.Member;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MemberRepository extends MongoRepository<Member, String> {
-
-}
