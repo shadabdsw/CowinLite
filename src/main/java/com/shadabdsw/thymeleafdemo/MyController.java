fd59d629d095ed1a12e9170fd6075b2603e055fb
Shadab Akhtar <shadab1996@gmail.com>
2022-02-03 19:56:38 +0530

    .

--- src/main/java/com/shadabdsw/thymeleafdemo/MyController.java
+++ src/main/java/com/shadabdsw/thymeleafdemo/MyController.java
@@ -32,7 +32,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
