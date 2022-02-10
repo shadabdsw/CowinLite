33fedd7efa432d5f9e7de1ba5051f1002d4344f0
Shadab Akhtar <shadab1996@gmail.com>
2022-02-10 14:17:59 +0530

    formatted document

--- src/main/java/com/shadabdsw/thymeleafdemo/Model/AddMemberReq.java
+++ src/main/java/com/shadabdsw/thymeleafdemo/Model/AddMemberReq.java
@@ -9,18 +9,21 @@ public class AddMemberReq {
         this.phoneNumber = phoneNumber;
         this.member = member;
     }
-    
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
     public Member getMember() {
         return member;
     }
+
     public void setMember(Member member) {
         this.member = member;
     }
+
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-    
+
 }
