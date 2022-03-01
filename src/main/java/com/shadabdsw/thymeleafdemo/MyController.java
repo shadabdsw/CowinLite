e2a8581d19e2f762b9630630b787c283a62b5b33
Shadab Akhtar <shadab1996@gmail.com>
2022-03-01 19:56:23 +0530

    updated login url

--- src/main/java/com/shadabdsw/thymeleafdemo/MyController.java
+++ src/main/java/com/shadabdsw/thymeleafdemo/MyController.java
@@ -26,14 +26,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
@@ -67,9 +65,9 @@ public class MyController {
         
         try {
             ResponseEntity<User> response = restTemplate
-                    .postForEntity("http://localhost:8081/registration/login?phoneNumber=" + user.getPhoneNumber() +
+                    .postForEntity("http://localhost:8081/registration/loginUser?phoneNumber=" + user.getPhoneNumber() +
                             "&password=" + user.getPassword(), user, User.class);
-            System.out.println("ResponseBody - " + response.getBody());
+            System.out.println("ResponseBody - " + response.getBody()); 
             u = (User) response.getBody();
             model.addAttribute("user", u);
 
