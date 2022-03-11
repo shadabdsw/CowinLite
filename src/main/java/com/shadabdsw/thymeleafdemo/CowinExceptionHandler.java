399fe2fe60cfe2c1188fca0659c16e10508c96d0
Shadab Akhtar <shadab1996@gmail.com>
2022-03-11 15:48:48 +0530

    CowinExceptionHandler changes

--- src/main/java/com/shadabdsw/thymeleafdemo/CowinExceptionHandler.java
+++ src/main/java/com/shadabdsw/thymeleafdemo/CowinExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.shadabdsw.thymeleafdemo;
 
+import com.shadabdsw.thymeleafdemo.Model.User;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,20 +13,26 @@ public class CowinExceptionHandler {
     public String handleAnyException(Exception ex, Model model) {
         System.out.println("Exception occured: " + ex.getMessage());
 
-        if (ex.getMessage().contains("403")) {
-            model.addAttribute("error", "Incorrect Password");
-            return "error/403";
+        model.addAttribute("error", ex.getMessage());
+
+        if (ex.getMessage().contains("401")) {
+            // model.addAttribute("error", "Unauthorized!");
+            return "error/401";
+        } else if (ex.getMessage().contains("403")) {
+            model.addAttribute("user", new User());
+            return "register";
         } else if (ex.getMessage().contains("404")) {
-            model.addAttribute("error", "Page not found");
-            return "error/404";
+            model.addAttribute("user", new User());
+            // model.addAttribute("error", "User not found!");
+            return "register";
         } else if (ex.getMessage().contains("405")) {
-            model.addAttribute("error", "Method not allowed");
+            // model.addAttribute("error", "Method not allowed!");
             return "error/405";
         } else if (ex.getMessage().contains("409")) {
-            model.addAttribute("error", "Conflict");
+            // model.addAttribute("error", "User already exists!");
             return "error/409";
         } else {
-            model.addAttribute("error", "Something went wrong");
+            // model.addAttribute("error", "Something went wrong!");
             return "error/500";
         }
     }
