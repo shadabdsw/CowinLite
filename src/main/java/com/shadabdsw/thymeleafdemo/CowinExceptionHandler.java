4e9f641401122805db5ee316fa4c1dd4ad0f988f
Shadab Akhtar <shadab1996@gmail.com>
2022-03-11 15:51:14 +0530

    removed comments

--- src/main/java/com/shadabdsw/thymeleafdemo/CowinExceptionHandler.java
+++ src/main/java/com/shadabdsw/thymeleafdemo/CowinExceptionHandler.java
@@ -16,23 +16,18 @@ public class CowinExceptionHandler {
         model.addAttribute("error", ex.getMessage());
 
         if (ex.getMessage().contains("401")) {
-            // model.addAttribute("error", "Unauthorized!");
             return "error/401";
         } else if (ex.getMessage().contains("403")) {
             model.addAttribute("user", new User());
             return "register";
         } else if (ex.getMessage().contains("404")) {
             model.addAttribute("user", new User());
-            // model.addAttribute("error", "User not found!");
             return "register";
         } else if (ex.getMessage().contains("405")) {
-            // model.addAttribute("error", "Method not allowed!");
             return "error/405";
         } else if (ex.getMessage().contains("409")) {
-            // model.addAttribute("error", "User already exists!");
-            return "error/409";
+            return "register";
         } else {
-            // model.addAttribute("error", "Something went wrong!");
             return "error/500";
         }
     }
