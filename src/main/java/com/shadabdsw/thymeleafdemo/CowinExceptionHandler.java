7daf154e5646883a69fda31ea25fcc84c444c339
Shadab Akhtar <shadab1996@gmail.com>
2022-03-21 14:48:48 +0530

    changes in CowinExceptionHandler

--- src/main/java/com/shadabdsw/thymeleafdemo/CowinExceptionHandler.java
+++ src/main/java/com/shadabdsw/thymeleafdemo/CowinExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.shadabdsw.thymeleafdemo;
 
+import com.shadabdsw.thymeleafdemo.Model.ErrorHandler;
 import com.shadabdsw.thymeleafdemo.Model.User;
 
 import org.springframework.ui.Model;
@@ -9,33 +10,36 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class CowinExceptionHandler {
 
-    @ExceptionHandler(value = { Exception.class })
+    ErrorHandler error = new ErrorHandler();
+
+    @ExceptionHandler(Exception.class)
     public String handleAnyException(Exception ex, Model model) {
         System.err.println("Exception occured: " + ex.getMessage());
 
-
-        // ExceptionMessage exceptionMessage = new ExceptionMessage(ex, ex.getMessage());
-        // System.out.println("Exception occured: " + exceptionMessage);
-        
         model.addAttribute("error", ex.getMessage());
 
         if (ex.getMessage().contains("401")) {
-            // model.addAttribute("error", "Unauthorized!");
             return "error/401";
         } else if (ex.getMessage().contains("403")) {
-            // model.addAttribute("error", "Incorrect Password!");
             model.addAttribute("user", new User());
+            error.setStatus(true);
+            error.setMessage("Incorrect Password!");
+            model.addAttribute("error", error);
             return "register";
         } else if (ex.getMessage().contains("404")) {
-            // model.addAttribute("error", "User not found!");
-            model.addAttribute("user", new User());
+            model.addAttribute("user", new User());        
+            error.setStatus(true);
+            error.setMessage("User Not Found!");
+            model.addAttribute("error", error);
             return "register";
         } else if (ex.getMessage().contains("405")) {
             // model.addAttribute("error", "Method not allowed!");
             return "error/405";
         } else if (ex.getMessage().contains("409")) {
-            // model.addAttribute("error", "User already exists!");
-            model.addAttribute("user", new User());
+            model.addAttribute("user", new User());        
+            error.setStatus(true);
+            error.setMessage("User Already Exists!");
+            model.addAttribute("error", error);
             return "register";
         } else {
             // model.addAttribute("error", "Something went wrong!");
@@ -43,5 +47,6 @@ public class CowinExceptionHandler {
         }
 
     }
+
 
 }
